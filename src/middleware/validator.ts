5edce42dd3a405d160b500cbb5ed8f7606879022
Summit Limbu <123644354+User23Sum@users.noreply.github.com>
2024-03-08 16:23:19 +0000

    Validator

---
+++ src/middleware/validator.ts
@@ -0,0 +1,18 @@
+import { param } from "express-validator";
+
+/**
+ * Validates the city name param
+ * @returns {ValidationChain} - Express validator validation chain
+ * @example
+ * router.get(
+ *  "/:city",
+ * validateCityName,
+ * getWeatherData
+ * );
+ */
+
+export const validateCityName = param("city")
+  .isString()
+  .isIn(["london", "dublin"]) // We will use the isIn method to check if the city param is either london or dublin
+  // We will use the withMessage method to set a custom error message
+  .withMessage("City name must be either london or dublin");
